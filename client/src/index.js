2035afc5c87d2033d5ee18e59e0fcacdb6fc2fa0
Yihua Zhang <michaelzhang2@msn.com>
2019-06-11 13:34:39 +0200

    adding appollo and graphql libraries

--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,34 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'https://hasura.crwn-clothing.com/v1/graphql'
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
